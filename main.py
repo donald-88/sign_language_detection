5ecc5dfa419ef87c0c0c035afe4034ec609247d9
NxtGenLabs <94043524+donald-88@users.noreply.github.com>
2023-01-26 21:14:52 +0200

    setup opencv camera

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("a python project for hand sign detection")
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    cv2.imshow('hsd', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
+        break
